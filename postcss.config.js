1ee4f29b4a17edf41858704618e81f4a435a2d68
alexisben <alexiben7@gmail.com>
2022-05-06 09:42:15 +0200

    Fix purge

--- postcss.config.js
+++ postcss.config.js
@@ -7,9 +7,14 @@ var devPlugins = {},
         },
         '@fullhuman/postcss-purgecss': {
             content: [
+                // './hugo_stats.json',
                 './themes/**/*.html',
                 'layouts/**/*.html'
             ],
+            // defaultExtractor: (content) => {
+            //     let els = JSON.parse(content).htmlElements;
+            //     return els.tags.concat(els.classes, els.ids);
+            // },
             safelist: {
                 standard: [
                     'show',
@@ -51,6 +56,12 @@ var devPlugins = {},
                     /^gclose/,
                     /^gbtn/,
                     /^gcontainer/
+                ],
+                greedy: [
+                    /__home/,
+                    /__page/,
+                    /__section/,
+                    /__term/
                 ]
             }
         }
